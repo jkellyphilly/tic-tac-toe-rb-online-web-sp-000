fccdb40f97cbf9641c6ce631174bed6ff7d420c4
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-20 17:36:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,3 +9,12 @@ WIN_COMBINATIONS = [
   [6, 4, 2], # / diagonal
   [0, 4, 8] # \ diagonal
 ]
+
+# Create a method to display the board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
